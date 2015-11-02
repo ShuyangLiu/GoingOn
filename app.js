6bf38b2523756b671f1828047598551289f923ff
Shuyang <sliu44@u.rochester.edu>
2015-11-01 19:31:21 -0500

    bug version, going to fix later

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var app = express();
 var http = require('http').Server(app); // start an http server
 
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+
 // app.set('views', './views');
 
 // app.set('view engine', 'jade');
@@ -20,14 +26,72 @@ var connection = mysql.createConnection({
   password : 'Lsy950621!',  
   database : 'goingon'  
 });  
-connection.connect();  
+connection.connect(function(err){
+ if(!err) 
+ {
+     console.log("Database is connected ... \n\n");  
+ } 
+ else 
+ {
+     console.log("Error connecting database ... \n\n");  
+ }
+ });  
 
-connection.query('SELECT * FROM user', function(err, rows, fields)   
-{  
-  if (err) throw err;  
+
+app.post('users/',function (req, res)
+{
+	var body = req.body;
+	var myName = body.name;
+	var myPassword = body.password;
+
+	if(!myName || !myPassword)//no username or password
+	{
+		res.send('{}');
+      	return;
+	}
+	else
+	{
+		//check database to see if the user name and password are correct
+		//use MySQL 
+
+		connection.query("SELECT * FROM user WHERE username=myName AND password=myPassword", 
+			function(err, rows, fields)
+		{
+
+			if(err)
+			{
+				res.send('{}');
+				return;
+			}
+			else
+			{
+				if(!rows[0])
+				{
+					res.send('{}');
+					return;
+				}
+				else
+				{
+					var a = {name: rows[0].username,
+						gender: rows[0].gender,
+						email: rows[0].email};
+
+					res.send(a);
+					return;
+				}
+			}
+		});
+	}
+});
+
+
+
+// connection.query('SELECT * FROM user', function(err, rows, fields)   
+// {  
+//   if (err) throw err;  
   
-  console.log(rows[0]);  
-});  
+//   console.log(rows[0]);  
+// });  
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
