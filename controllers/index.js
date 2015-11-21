b6083e783bffa50aa35c699388233a3051cefe17
Xie Haozhe <zjhzxhz@gmail.com>
2015-11-21 09:37:08 +0800

    Add the handler for /

--- controllers/index.js
+++ controllers/index.js
@@ -6,20 +6,19 @@ var express         = require('express'),
     router          = express.Router();
 
 router.use('/accounts', require('./accounts'));
-app.get('/', function(request, response) {
+app.get('/home', function(request, response) {
     var sess = request.session;
 
-    console.log('[DEBUG] Get /');
+    console.log('[DEBUG] Get /home');
 
     if( sess.username ) {
-      console.log('[DEBUG] found username in session: ' + sess.username);
-      response.redirect('/profile');
+        console.log('[DEBUG] found username in session: ' + sess.username);
+        response.redirect('/profile');
     } else {
-      console.log('[DEBUG] Cannot find username in session redirecting to index.html');
+        console.log('[DEBUG] Cannot find username in session redirecting to index.html');
     }
 
-    // response.render('index.html');
-    response.end("/");
+    response.render('misc/home.html');
 });
 
 module.exports = router;
